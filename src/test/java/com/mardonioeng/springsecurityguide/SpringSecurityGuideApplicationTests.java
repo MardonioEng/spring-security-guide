e0ffd64589200e4ca0cda8384e7984d87ee3c1d0
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2022-04-08 08:48:00 -0300

    Projeto criado

---
+++ src/test/java/com/mardonioeng/springsecurityguide/SpringSecurityGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardonioeng.springsecurityguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
